d13ad19eb68e67ca3a7f78a09262599437623727
Mitch Hammond <mitch.b.hammond@gmail.com>
2024-02-12 12:57:18 -0700

    updates to quotes provider

--- frontend/context/quotesContext.js
+++ frontend/context/quotesContext.js
@@ -1,1 +1,17 @@
 // Create your context here
+import React, {createContext} from 'react'
+
+export const QuotesContext = createContext ()
+
+const avlue = {
+    car: 'Toyota Celica'
+}
+
+export function QuotesProvider(props){
+
+    return (
+        <QuotesContext.Provider value={value}>
+            {props.children}
+            </QuotesContext.Provider>
+    )
+}
